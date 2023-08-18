552f5c96a9eef6354ed619bdd591991117ad9be1
Jorge Cuadrado Dorrio <jcuador@upv.edu.es>
2023-08-18 13:23:04 +0200

    initial commit

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ app.use(cors())
 app.use(express.json())
 app.use('/comidas', router)
 app.use('/.netlify/functions/api', router)
-app.use(function(req, response, next) {
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Credentials", "true");
-    response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-})
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 
 //Realiza la conexion con la base de datos
 try {
