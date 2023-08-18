d0ed675c78f39404b3800ca45de3ab1df4502a75
Jorge Cuadrado Dorrio <jcuador@upv.edu.es>
2023-08-18 13:27:43 +0200

    initial commit

--- app.js
+++ app.js
@@ -12,12 +12,11 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use('/comidas', router)
-app.use('/.netlify/functions/api', router)
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
   });
 
